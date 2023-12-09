5ba54346ef45f96310a8015acd3694521d09730a
alviasad0 <alviasad10.com>
2023-12-10 03:11:46 +0600

    added the menu button in the navbar

--- src/Components/SideBar/Components/TroggleButton.jsx
+++ src/Components/SideBar/Components/TroggleButton.jsx
@@ -1,10 +1,17 @@
-
-const TroggleButton = ({ setOpen}) => {
-    return (
-      <div>
-        <button onClick={()=> setOpen((prev)=> !prev)} className="w-12 h-12 rounded-full fixed top-6 left-6 bg-transparent border-none"> hi</button>
-      </div>
-    );
+import { HiMenuAlt1 } from "react-icons/hi";
+// eslint-disable-next-line react/prop-types
+const TroggleButton = ({ setOpen }) => {
+  return (
+    <div>
+      <button
+        onClick={() => setOpen((prev) => !prev)}
+        className="w-12 h-12 rounded-full fixed top-6 left-6 bg-transparent border-none"
+      >
+        {" "}
+        <HiMenuAlt1 className="text-2xl font-extrabold mx-auto"></HiMenuAlt1>
+      </button>
+    </div>
+  );
 };
 
 export default TroggleButton;
