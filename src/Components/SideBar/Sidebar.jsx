2f6b7d2813b7638ab541a63a38eaad4799e33225
alviasad0 <alviasad10.com>
2023-12-11 00:04:00 +0600

    made the navbar button functional

--- src/Components/SideBar/Sidebar.jsx
+++ src/Components/SideBar/Sidebar.jsx
@@ -2,28 +2,30 @@ import { useState } from "react";
 import Links from "./Components/Links";
 import TroggleButton from "./Components/TroggleButton";
 import { motion } from 'framer-motion';
+import { HiMenuAlt1 } from "react-icons/hi";
+import { IoMdCloseCircle } from "react-icons/io";
 
+const variants = {
+  open: {
+    clipPath: "circle(1200px at 50px 50px)",
+    transition: {
+      type: "spring",
+      stiffness: 20,
+    },
+  },
+  closed: {
+    clipPath: "circle(30px at 50px 50px)",
+    transition: {
+      delay: 0.5,
+      type: "spring",
+      stiffness: 400,
+      damping: 40,
+    },
+  },
+};
 const Sidebar = () => {
   const [open, setOpen] = useState(false);
 
-  const variants = {
-    open: {
-      clipPath: "circle(1200px at 50px 50px)",
-      transition: {
-        type: "spring",
-        stiffness: 20,
-      },
-    },
-    closed: {
-      clipPath: "circle(30px at 50px 50px)",
-      transition: {
-        delay: 0.5,
-        type: "spring",
-        stiffness: 400,
-        damping: 40,
-      },
-    },
-  };
 
   return (
     <motion.div
@@ -31,12 +33,24 @@ const Sidebar = () => {
       className="flex flex-col items-center justify-center bg-white text-black"
     >
       <motion.div
-        variants={variants} 
+        variants={variants}
         className="fixed top-0 left-0 bottom-0 w-[400px] bg-white"
       >
         <Links />
       </motion.div>
-      <TroggleButton setOpen={setOpen} />
+      <div>
+        <button
+          onClick={() => setOpen((prev) => !prev)}
+          className="w-12 h-12 rounded-full fixed top-6 left-6 bg-transparent border-none"
+        >
+          {" "}
+          {!open ? (
+            <HiMenuAlt1 className="text-2xl font-extrabold mx-auto"></HiMenuAlt1>
+          ) : (
+            <IoMdCloseCircle className="text-2xl font-extrabold mx-auto"></IoMdCloseCircle>
+          )}
+        </button>
+      </div>
     </motion.div>
   );
 };
